326c777899ea7aa8daacda3a7c137f70b21c613c
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-04-22 14:04:09 +0530

    Create a1.find smallest element in array.c

---
+++ a1.find smallest element in array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int  smallest_ele (int a[], int size)
+{
+    int small = a[0];
+
+    for(int i =1; i<size; i++)
+    {
+        if(small > a[i])
+        {
+            small = a[i];
+        }
+    }
+    return small;
+}
+
+
+int main()
+{
+    int size = 4;
+    int a[] = {4, 8, 0, 5};
+
+    int result = smallest_ele(a, size);
+    printf("smallest element is = %d", result);
+    return 0;
+}
