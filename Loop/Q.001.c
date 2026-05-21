e8031e74cb3d42ac8e28cdf949b1405881bd3295
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-05-21 23:20:20 +0530

    Create Q.001.c

---
+++ Loop/Q.001.c
@@ -0,0 +1,14 @@
+//Print all numbers from 1 to 10 using a loop.
+
+#include <stdio.h>
+int main()
+{
+    int i=0;
+
+    while( i <= 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
