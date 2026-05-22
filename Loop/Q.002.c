f1d763a36e9252c73d29e7b6adb02aa58b93f0c0
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-05-22 14:24:50 +0530

    Create Q.002.c

---
+++ Loop/Q.002.c
@@ -0,0 +1,14 @@
+//Print numbers from 10 down to 1 in reverse order. 
+
+#include <stdio.h>
+int main()
+{
+    int i=10;
+
+    while( i >= 0)
+    {
+        printf("%d\n", i);
+        i=i-1;
+    }
+    return 0;
+}
