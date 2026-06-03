ef2cd4ef31d27b497e7b3d71b315f76f86ca22fe
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-06-03 21:03:30 +0530

    Create Q.007.c

---
+++ Loop/Q.007.c
@@ -0,0 +1,21 @@
+//Calculate the sum of all even numbers from 1 up to n. 
+#include <stdio.h>
+int main()
+{
+    
+    int sum = 0;
+    int num = 0;
+    printf("Enter Number = ");
+    scanf("%d", &num);
+
+    while(num >= 0)
+    {
+        if(num%2 == 0)
+        {
+            sum = sum + num; 
+        }
+         num = num -1;
+    }
+    printf("Sum is = %d", sum);
+    return 0;
+} 
